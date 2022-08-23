07fa7241907a1e9d5b66aff1c44ffed3780bd993
mzltest <52124882+mzltest@users.noreply.github.com>
2022-08-23 20:14:19 +0800

    Create index.js

---
+++ src/http/get-index/index.js
@@ -0,0 +1,31 @@
+exports.handler = async function read(req) {
+
+  bodytext=`
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width">
+<title>Max. ~10kb</title>
+<body>
+<form action="/new" method="POST">
+  <textarea name="text" rows="10" cols="30"></textarea>
+  <input type="submit" value="Submit">
+  </form> 
+  
+</body>
+</html>
+`
+
+
+
+
+       return {
+  statusCode: 200,
+  headers: {
+    'content-type': 'text/html; charset=utf8',
+    'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+  }, body: bodytext
+}}
+
+
