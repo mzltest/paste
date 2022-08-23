91a235e1f63f731894c5f58a5d5fe6243d948611
mzltest <52124882+mzltest@users.noreply.github.com>
2022-08-23 08:29:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,23 @@
-exports.handler = async function http(req) {
-
-  let html = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
-  </head>
-  <body>
-
-    <h1 class="center-text">
-      <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
-    </h1>
-
-    <p class="center-text">
-      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
-    </p>
-
-  </body>
-</html>`
+exports.handler = async function resp(req) {
+ if (req.rawPath=='/'){
 
   return {
     headers: {
       'content-type': 'text/html; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
-    statusCode: 200,
-    body: html
+    statusCode: 404,
+    body: 'append dest url after path'
+  }}
+  else{
+    url=req.rawPath
+    
+  
+  
+  
+  
+  
+  
   }
 }
 
