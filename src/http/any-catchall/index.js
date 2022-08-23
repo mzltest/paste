84b301697fbc655d83a9dfe4969e0122cd96ec3d
mzltest <52124882+mzltest@users.noreply.github.com>
2022-08-23 10:54:28 +0800

    Update index.js

--- src/http/any-catchall/index.js
+++ src/http/any-catchall/index.js
@@ -22,15 +22,13 @@ exports.handler = async function resp(req) {
   
   else{
   console.log(req.rawPath)
-    url="https://google.com"
+    url="https://baidu.com"
    cookies=req.cookies
    headers=req.headers
  headers.host=url.split('/')[1] //set the correct host
-   body=null?req.requestContext.http.method=='GET': parseBody(req)
-console.log(cookies)
-    console.log(headers)
+
 const response = await fetch(url,{method:req.requestContext.http.method,headers:headers,agent: httpsAgent});
-console.log(response.text())
+
     return {
     headers:response.headers.raw(),
     statusCode: response.status,
