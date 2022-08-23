00170b7211f704f2f5c0fb19a244b7979e32fff9
mzltest <52124882+mzltest@users.noreply.github.com>
2022-08-23 17:59:53 +0800

    Update index.js

--- src/http/any-catchall/index.js
+++ src/http/any-catchall/index.js
@@ -1,44 +1,38 @@
-const https = require('https');
-let arc = require('@architect/functions')
-let parseBody = arc.http.helpers.bodyParser
-const fetch=require("node-fetch");
-const httpsAgent = new https.Agent({
-  rejectUnauthorized: false,
-});
+const data = require('@begin/data')
 
-exports.handler = async function resp(req) {
- if (req.rawPath=='/'){
-
+exports.handler = async function read(req) {
+  if (req.path=='/'){
   return {
+    statusCode: 201,
     headers: {
-      'content-type': 'text/html; charset=utf8',
+      'content-type': 'text/plain; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
+    body: 'post /new'
+  }
+  }
+  
+  
+  
+  try{
+  let ans = await data.get({
+    table: 'todos',
+    key:req.path.substring(1)
+  })}
+catch{ return {
     statusCode: 404,
-    body: 'append dest url after path'
-  }}
-
-  else{
-    console.log(req.headers)
- url='http'+req.headers['referer']
- // url=url.substring(url.length - 1)
-  
-  
-                            return {
-    headers:{ 'content-type': 'text/html; charset=utf8',
+    headers: {
+      'content-type': 'text/plain; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-},
-    statusCode:200,
-    body: url
+    }
+  }}
+ 
+  return {
+    statusCode: 201,
+    headers: {
+      'content-type': 'text/plain; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body: ans.text
   }
-   
-  }
-  
-  
-  
-  
-  
-  
 }
-
-
