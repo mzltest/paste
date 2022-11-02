573b2617990a888d750f3ca312bc00125a3f9659
mzltest <52124882+mzltest@users.noreply.github.com>
2022-11-02 18:46:07 +0800

    Update index.js

--- src/http/post-catchall/index.js
+++ src/http/post-catchall/index.js
@@ -23,7 +23,7 @@ exports.handler = async function create(req) {
     headers: {
       
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    }
+    },
      body:res.key
   }
 }
